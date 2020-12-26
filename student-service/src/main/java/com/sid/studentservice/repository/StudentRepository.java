d175e2d65462d93439f0057ab1c030c16b39fbe7
ouma-h <hannachioumeima007@gmail.com>
2020-12-26 23:09:42 +0100

    added config to local postgresql database

--- student-service/src/main/java/com/sid/studentservice/repository/StudentRepository.java
+++ student-service/src/main/java/com/sid/studentservice/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.sid.studentservice.repository;
 
 import com.sid.studentservice.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
