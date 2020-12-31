742a40a000aec1cf4378a8a34d0c282991f2ded2
ouma-h <hannachioumeima007@gmail.com>
2020-12-31 16:19:59 +0100

    updated proxy

--- proxy-service/src/main/java/com/sid/proxyservice/ProxyServiceApplication.java
+++ proxy-service/src/main/java/com/sid/proxyservice/ProxyServiceApplication.java
@@ -3,11 +3,10 @@ package com.sid.proxyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProxyServiceApplication {
 
