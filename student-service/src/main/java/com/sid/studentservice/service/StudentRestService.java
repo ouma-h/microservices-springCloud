ac5e664919c77081ef1680d395e8e3146403e37e
ouma-h <hannachioumeima007@gmail.com>
2021-02-04 14:48:13 +0100

    added results api

--- student-service/src/main/java/com/sid/studentservice/service/StudentRestService.java
+++ student-service/src/main/java/com/sid/studentservice/service/StudentRestService.java
@@ -1,20 +1,25 @@
 package com.sid.studentservice.service;
 
+import com.sid.studentservice.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RefreshScope
 @RestController
 public class StudentRestService {
-    @Value("kita hob")
-    private String message;
+    @Autowired
+    private StudentRepository studentRepository;
 
-    @RequestMapping("/messages")
-    public String tellMe() {
-        System.out.println("###############   ASSLEEEEEEEEEEEMA");
-        return message;
+    @RequestMapping("/results")
+    public Object tellMe() {
+        List<Object[]> objects = studentRepository.getstudentsbyresults();
+        return ResponseEntity.ok().body(objects);
     }
 
 }
